869f763f1ff9129de84029aacc1ec7fe2518e4c2
John Scarrott <johnps@outlook.com>
2023-04-20 16:53:46 +0100

    feat: Added logic to allow combining of mdbook, serialization still not done

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,83 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use mdbook::book::{Summary};
+use clap::Parser;
+use mdbook::book::Summary;
+use walkdir::WalkDir;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the meta directory containing all the summary files
+    #[arg(short, long)]
+    meta_directory: PathBuf,
+}
 
 fn main() {
-    let paths = fs::read_dir("./test").unwrap();
-    let summaries: Vec<String> = paths
+    let config = Args::parse();
+    let paths = WalkDir::new(&config.meta_directory);
+    let summaries: Vec<(String, PathBuf)> = paths
         .into_iter()
         .filter(|x| x.as_ref().unwrap().file_name().to_str().unwrap() == "SUMMARY.md")
-        .map(|x| fs::read_to_string(x.unwrap().path()).unwrap())
+        .map(|x| {
+            (
+                fs::read_to_string(x.as_ref().unwrap().path()).unwrap(),
+                x.unwrap().path().parent().unwrap().to_path_buf(),
+            )
+        })
         .collect();
     let length = summaries.len();
 
-    let sum = mdbook::book::parse_summary(&summaries[0]).unwrap();
-    println!("{sum:#?}");
-    println!("{length}");
-    let path = Path::new("./new_path");
-    rebase_summary(path, sum);
+    let rebased_summaries: Vec<Summary> = summaries
+        .iter()
+        .map(|x| {
+            let absolute_path = &x.1.canonicalize().unwrap();
+            let sum = mdbook::book::parse_summary(&x.0).unwrap();
+            rebase_summary(absolute_path, sum)
+        })
+        .collect();
+
+    let final_summary = rebased_summaries
+        .into_iter()
+        .fold(Summary::default(), |mut acc, mut x| {
+            acc.prefix_chapters.append(&mut x.prefix_chapters);
+            acc.numbered_chapters.append(&mut x.numbered_chapters);
+            acc.suffix_chapters.append(&mut x.suffix_chapters);
+            acc
+        });
+
+    println!("{final_summary:#?}");
+    std::fs::write("Summary.md", final_summary.display());
+    // println!("{length}");
+    // let absolute_path = &summaries[0].1.canonicalize().unwrap();
+    // rebase_summary(&absolute_path, sum);
 }
 
 fn rebase_summary(new_base: &Path, summary: Summary) -> Summary {
-    let new_prefix: Vec<mdbook::book::SummaryItem> = summary
+    let new_numbered: Vec<mdbook::book::SummaryItem> = summary
         .numbered_chapters
         .into_iter()
         .map(|x| rebase(x, new_base))
         .collect();
-    println!("{new_prefix:#?}");
 
-    todo!()
+    let new_prefix: Vec<mdbook::book::SummaryItem> = summary
+        .prefix_chapters
+        .into_iter()
+        .map(|x| rebase(x, new_base))
+        .collect();
+
+    let new_suffix: Vec<mdbook::book::SummaryItem> = summary
+        .suffix_chapters
+        .into_iter()
+        .map(|x| rebase(x, new_base))
+        .collect();
+    Summary {
+        prefix_chapters: new_prefix,
+        numbered_chapters: new_numbered,
+        suffix_chapters: new_suffix,
+        title: summary.title,
+    }
+    // todo!()
 }
 
 // fn rebase(mut sum_item: SummaryItem, new_base: &Path) -> SummaryItem {
